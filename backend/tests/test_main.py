0774c288b7b41bdcb272eee181eab6dbda978035
Lucas Martini <lucas.leite.martini@gmail.com>
2025-07-24 17:39:53 -0400

    feat(test): added tests for app.routes.map.map_router

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -1,15 +1,20 @@
 """TODO add comentários"""
 
-from fastapi.testclient import TestClient
 
-from app.main import app
-
-client = TestClient(app)
-
-
-def test_read_main():
+def test_read_main(client):
     """TODO add comentários"""
 
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World"}
+
+
+def test_search_location(client):
+    response = client.post("/map/ufam")
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "address": "Federal Univ of Amazonas - Av. General Rodrigo Octavio Jordão Ramos, 1200 - Coroado I, Manaus - AM, 69067-005, Brazil",
+        "lng": -59.9765378,
+        "lat": -3.1002425,
+    }
